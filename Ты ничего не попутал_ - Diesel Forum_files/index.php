f3f69d10fb49c0ba73a5880629d51ff741834441
akimov <akimov.infotech@gmail.com>
2022-06-06 21:24:28 +0600

    test

---
+++ Ты ничего не попутал_ - Diesel Forum_files/index.php
@@ -0,0 +1,52 @@
+.pln{color:#000}@media
+screen{.str{color:#080}.kwd{color:#008}.com{color:#800}.typ{color:#606}.lit{color:#066}.pun,.opn,.clo{color:#660}.tag{color:#008}.atn{color:#606}.atv{color:#080}.dec,.var{color:#606}.fun{color:red}}@media print,projection{.str{color:#060}.kwd{color:#006;font-weight:bold}.com{color:#600;font-style:italic}.typ{color:#404;font-weight:bold}.lit{color:#044}.pun,.opn,.clo{color:#440}.tag{color:#006;font-weight:bold}.atn{color:#404}.atv{color:#060}}.post.entry-content pre ol, .post_body pre ol, .as_content pre ol, .comment_content pre ol,ol.linenums{margin-left:40px !important;margin-top:0 !important;margin-bottom:0 !important;color:#AEAEAE !important}li.L0,li.L1,li.L2,li.L3,li.L4,li.L5,li.L6,li.L7,li.L8,li.L9{list-style-type:decimal !important;list-style-position:outside !important;padding:2px
+0 !important}li.L1,li.L3,li.L5,li.L7,li.L9{background-color:#f5f5f5}@media
+print{pre .str, code
+.str{color:#060}pre .kwd, code
+.kwd{color:#006;font-weight:bold}pre .com, code
+.com{color:#600;font-style:italic}pre .typ, code
+.typ{color:#404;font-weight:bold}pre .lit, code
+.lit{color:#044}pre .pun, code
+.pun{color:#440}pre .pln, code
+.pln{color:#000}pre .tag, code
+.tag{color:#006;font-weight:bold}pre .atn, code
+.atn{color:#404}pre .atv, code
+.atv{color:#060}}.bbcode_hilight{background-color:yellow}.as_content{background:#fff;font-size:1.0em;border:1px
+solid black;padding:6px;margin:8px;overflow:auto;max-height:400px}.as_buttons{text-align:right;padding:4px
+0px}.as_message{display:inline-block}.ipsEditor_textarea{width:99%;height:200px;font-size:14px}.cke_browser_webkit{outline:none !important}.cke_top{background:#E4EBF2 url(https://diesel.elcat.kg/public/style_images/master/editor/toolbar_bg.png) repeat-x !important}.cke_skin_ips
+textarea.cke_source{}.cke_skin_ips
+.cke_wrapper.minimized{opacity:0.6 !important;background:none !important;border:none !important}.cke_skin_ips
+.cke_wrapper.minimized.std{border:2px
+solid #D5DDE5 !important}.cke_skin_ips{margin-bottom:0px !important}.cke_skin_ips
+.cke_wrapper{padding:0px
+5px 0px 3px !important;border:2px
+solid #D5DDE5 !important;background-color:#E4EBF2 !important;background-image:none !important}.cke_skin_ips
+.cke_toolgroup{background-color:transparent !important}.cke_skin_ips .cke_button a:hover,
+.cke_skin_ips .cke_button a:focus,
+.cke_skin_ips .cke_button a:active{background-color:#d5dde5 !important}.cke_skin_ips .cke_button a:hover.cke_on,
+.cke_skin_ips .cke_button a:focus.cke_on,
+.cke_skin_ips .cke_button a:active.cke_on{background-color:#86caff !important}.cke_skin_ips
+.cke_toolgroup{margin-right:0px !important}.cke_skin_ips
+.cke_separator{border-left:solid 1px #D5DDE5;display:inline-block !important;float:left;height:30px;margin:0px
+2px}.cke_dialog_background_cover{background-color:#3e3e3e !important}.cke_skin_ips
+.cke_dialog_title{background:#2c5687 url(https://diesel.elcat.kg/public/style_images/master/maintitle.png) repeat-x top !important;color:#fff !important;padding:10px
+10px 11px !important;font-size:16px !important;font-weight:300 !important;text-shadow:0 1px 2px rgba(0,0,0,0.3);font-weight:normal}.cke_skin_ips
+.cke_dialog_body{z-index:20000 !important}.cke_skin_ips
+.cke_dialog_tabs{background:#2C5687 !important}.cke_skin_ips
+.cke_dialog_close_button{background:transparent url(https://diesel.elcat.kg/public/style_images/master/close_popup.png) no-repeat top left !important;width:13px !important;height:13px !important;top:11px !important;right:10px !important}.cke_skin_ips
+span.cke_dialog_ui_button{height:22px !important;line-height:22px !important;font-size:12px !important;color:#7c7c7c !important;padding:0
+10px !important;background:#f6f6f6 !important;background:-moz-linear-gradient(top, #f6f6f6 0%, #e5e5e5 100%) !important;background:-webkit-gradient(linear, left top, left bottom, color-stop(0%,#f6f6f6), color-stop(100%,#e5e5e5)) !important;border:1px
+solid #dbdbdb !important;-moz-box-shadow:0px 1px 0px rgba(255,255,255,1) inset, 0px 1px 0px rgba(0,0,0,0.3) !important;-webkit-box-shadow:0px 1px 0px rgba(255,255,255,1) inset, 0px 1px 0px rgba(0,0,0,0.3) !important;box-shadow:0px 1px 0px rgba(255,255,255,1) inset, 0px 1px 0px rgba(0,0,0,0.3) !important;-moz-border-radius:3px !important;-webkit-border-radius:3px !important;border-radius:3px !important;color:#616161 !important;display:inline-block !important;white-space:nowrap !important}.cke_skin_ips .cke_dialog_footer
+.cke_resizer{display:none}.ipsSmileyTray{position:relative;text-align:center;overflow:auto;margin:0px
+auto 0px auto;padding:4px
+24px 4px 24px;min-width:600px;width:75%;height:32px;border:1px
+solid #D5DDE5;-moz-border-radius:3px;-webkit-border-radius:3px;border-radius:3px;-moz-border-radius-topleft:0px;-webkit-border-radius-topleft:0px;border-top-left-radius:0px;-moz-border-radius-topright:0px;-webkit-border-radius-topright:0px;border-top-right-radius:0px;border-top:0px;-moz-box-shadow:inset 0 1px 0 0 #eff3f8, 0px 2px 3px rgba(0,0,0,0.2);-webkit-box-shadow:inset 0 1px 0 0 #eff3f8, 0px 2px 3px rgba(0,0,0,0.2);box-shadow:inset 0 1px 0 0 #eff3f8, 0px 2px 3px rgba(0,0,0,0.2);background:#E4EBF2;overflow-y:hidden}.ipsSmileyTray
+img.bbc_emoticon{opacity:0.8;cursor:pointer;margin:6px
+3px 0px 3px;max-width:30px;max-height:30px}.ipsSmileyTray img.bbc_emoticon:hover{opacity:1.0}.ipsSmileyTray
+.ipsSmileyTray_next{background:transparent url(https://diesel.elcat.kg/public/style_images/master/editor/next.png) no-repeat;background-position:0px 10px;display:inline-block;position:absolute;right:5px;top:4px;width:13px;height:30px;cursor:pointer}.ipsSmileyTray
+.ipsSmileyTray_prev{background:transparent url(https://diesel.elcat.kg/public/style_images/master/editor/prev.png) no-repeat;background-position:0px 10px;display:inline-block;position:absolute;left:5px;top:4px;width:13px;height:30px;cursor:pointer}.ipsSmileyTray_all{display:block;width:auto;margin:3px
+auto 0px auto;text-align:center;cursor:pointer;font-size:10px !important}.cke_dialog.cke_single_page
+td.cke_dialog_contents{height:auto !important}.cke_dialog
+.cke_dialog_ui_textarea{height:130% !important}table.cke_editor
+td{padding:0px
+!important}
